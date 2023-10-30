7c9413c2a937e14bdfcc809dd0e91626b3b97fda
Sandro Domianidze <sdomianidze123@gmail.com>
2023-10-30 20:09:14 +0400

    style: create date picker

---
+++ src/screens/Home/components/DatePicker/index.tsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import {View, ScrollView, Pressable, Text} from 'react-native';
+import moment from 'moment';
+
+type TProps = {
+  activeDate: moment.Moment;
+  setActiveDate: React.Dispatch<React.SetStateAction<moment.Moment>>;
+};
+
+const DatePicker: React.FC<TProps> = ({activeDate, setActiveDate}) => {
+  const scrollViewRef = React.useRef<ScrollView>(null);
+
+  const [dates, setDates] = React.useState<moment.Moment[]>([]);
+
+  const datePressHandler = (date: moment.Moment) => {
+    setActiveDate(date);
+
+    if (scrollViewRef.current) {
+      const dateIndex = dates.findIndex(item => item.date() === date.date());
+
+      scrollViewRef.current.scrollTo({
+        x: 112 * (dateIndex - 1),
+        y: 0,
+        animated: true,
+      });
+    }
+  };
+
+  React.useEffect(() => {
+    const updatedDates = [...new Array(7)].map((_, index) =>
+      index > 0 ? moment().add(index, 'days') : moment(),
+    );
+
+    setDates(updatedDates);
+  }, []);
+
+  return (
+    <View className="p-2">
+      <Text className="pb-2 text-lg text-white font-bold">
+        {activeDate.format('MMMM, Do, YYYY.')}
+      </Text>
+      <ScrollView
+        className="flex-row gap-4"
+        horizontal={true}
+        showsHorizontalScrollIndicator={false}
+        ref={scrollViewRef}>
+        {dates.map(item => {
+          const isActive = item.date() === activeDate.date();
+
+          return (
+            <Pressable
+              className={`h-24 w-24 justify-center items-center rounded-xl ${
+                isActive ? 'bg-blue-500' : 'bg-white'
+              }`}
+              onPress={datePressHandler.bind(this, item)}
+              key={item.toString()}>
+              <Text className="text-xl font-bold">{item.date()}</Text>
+            </Pressable>
+          );
+        })}
+      </ScrollView>
+    </View>
+  );
+};
+
+export default DatePicker;
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import {View} from 'react-native';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import moment from 'moment';
 import {StackParamList} from '@root/App';
+import DatePicker from './components/DatePicker';
 
 type TProps = NativeStackScreenProps<StackParamList, 'home'>;
 
 const HomeScreen: React.FC<TProps> = () => {
-  return <></>;
+  const [activeDate, setActiveDate] = React.useState<moment.Moment>(moment());
+
+  return (
+    <View>
+      <DatePicker activeDate={activeDate} setActiveDate={setActiveDate} />
+    </View>
+  );
 };
 
 export default HomeScreen;
