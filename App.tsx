e369f2421fc0d09a4004f369c2438a219335320f
Sandro Domianidze <sdomianidze123@gmail.com>
2023-10-30 15:18:33 +0400

    feat: setup navigation

--- App.tsx
+++ App.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import {SafeAreaView, StatusBar} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import HomeScreen from './src/screens/Home';
+
+export type StackParamList = {
+  home: undefined;
+};
+
+const Stack = createNativeStackNavigator<StackParamList>();
 
 function App(): JSX.Element {
   return (
     <SafeAreaView className="h-full bg-black">
       <StatusBar barStyle="light-content" />
+      <NavigationContainer>
+        <Stack.Screen name="home" component={HomeScreen} />
+      </NavigationContainer>
     </SafeAreaView>
   );
 }
---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import {StackParamList} from '../../../App';
+
+type TProps = NativeStackScreenProps<StackParamList, 'home'>;
+
+const HomeScreen: React.FC<TProps> = () => {
+  return <></>;
+};
+
+export default HomeScreen;
