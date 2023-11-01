32d7668ad94d58ff370fb44a8da5c0ab6ad39388
Sandro Domianidze <sdomianidze123@gmail.com>
2023-11-01 15:25:40 +0400

    feat: setup vector icons

--- src/screens/Home/components/AddButton/index.tsx
+++ src/screens/Home/components/AddButton/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import {Pressable, Text} from 'react-native';
+import {Pressable} from 'react-native';
+import Entypo from 'react-native-vector-icons/Entypo';
 
 const AddButton: React.FC = () => {
   return (
     <Pressable className="absolute right-6 bottom-6 w-12 h-12 justify-center items-center bg-white rounded-xl">
-      <Text className="text-lg">+</Text>
+      <Entypo name="plus" size={24} />
     </Pressable>
   );
 };
